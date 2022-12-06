13bfb86575f39b9c105fbb26735dd2170ec068d2
Ankit Kushwaha <>
2022-12-06 06:27:05 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+a = {"a", "b", "c","1"}
+print(a)
+b= {"1","2","3"}
+print (a-b)
+print (a|b)
+print (a&b)
+print (a^b)
+
+
+daily={
+  'sun':'40',
+  'monday':'65',
+  'sunday':'65'}
+daily['sat']=36
+print (daily )
